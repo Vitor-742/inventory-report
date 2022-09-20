1aa4862cd3704b9f4b1917827d4456b1a347ead6
Vitor Campos <vitorcampos742@gmail.com>
2022-09-20 16:51:38 -0300

    req 1 teste

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import datetime
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto_test = Product(
+        3,
+        'produtoTeste',
+        'empresaTeste',
+        datetime.now(),
+        datetime.now(),
+        '10',
+        'em ambiente arejado'
+    )
+    assert type(produto_test.id) == int
+    assert type(produto_test.nome_do_produto) == str
+    assert type(produto_test.nome_da_empresa) == str
+    assert type(produto_test.data_de_fabricacao) == str
+    assert type(produto_test.data_de_validade) == str
+    assert type(produto_test.numero_de_serie) == str
+    assert type(produto_test.instrucoes_de_armazenamento) == str

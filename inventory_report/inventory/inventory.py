6c125041bf72fd1e97b42416eb846f001bd17630
Vitor Campos <vitorcampos742@gmail.com>
2022-09-21 20:02:29 -0300

    req6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,21 +1,38 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
+def reader_csv(file):
+    with open(file) as csv_file:
+        CSVreader = csv.reader(csv_file, delimiter=",", quotechar='"')
+        header, *data = CSVreader
+
+    listDataCSV = []
+
+    for item in data:
+        product = {}
+        for i in range(len(item)):
+            product.update({header[i]: item[i]})
+        listDataCSV.append(product)
+    return listDataCSV
+
+
+def reader_json(file):
+    with open(file) as json_file:
+        content = json_file.read()
+        return json.loads(content)
+
+
 class Inventory:
     def import_data(file, report_type):
-        with open(file) as csv_file:
-            CSVreader = csv.reader(csv_file, delimiter=",", quotechar='"')
-            header, *data = CSVreader
-
+        type_file = file.split('.')[-1]
         listData = []
-        for item in data:
-            product = {}
-            for i in range(len(item)):
-                product.update({header[i]: item[i]})
-            listData.append(product)
-
+        if(type_file == 'csv'):
+            listData = reader_csv(file)
+        if(type_file == 'json'):
+            listData = reader_json(file)
         if(report_type == "simples"):
             relatorio_simples = SimpleReport()
             return relatorio_simples.generate(listData)
